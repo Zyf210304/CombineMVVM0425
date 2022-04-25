b1e8818ee35f90f4ff996c2031162d2dcf7d290c
张亚飞 <672233580@qq.com>
2022-04-25 13:51:12 +0800

    Initial Commit

---
+++ CombineMVVM0425/CombineMVVM0425App.swift
@@ -0,0 +1,17 @@
+//
+//  CombineMVVM0425App.swift
+//  CombineMVVM0425
+//
+//  Created by 张亚飞 on 2022/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineMVVM0425App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
